9199ea0220c8de9595f668dd9b40cf6b08c3cf51
Osemeke Echenim <echenim.osemeke@gmail.com>
2021-08-20 17:44:35 +0100

    fixed time output

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
 const consola = require('consola')
+const formatMessage = require('./utils/messages')
 
 
 const app = express()
@@ -12,17 +13,24 @@ const io = socketio(server)
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+const botName = 'ChatCord Bot'
+
 //Run when a client connects
 io.on('connection', socket => {
   //Welcome current user
-  socket.emit('message', 'Welcome to Chatting!!!')
+  socket.emit('message', formatMessage(botName, 'Welcome to Chatting!!!'))
 
   //Broadcast when a user connects
-  socket.broadcast.emit('message', 'A user has joined the chat')
+  socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'))
 
   //Runs when a client disconnects
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat')
+    io.emit('message', formatMessage(botName, 'A user has left the chat'))
+  })
+
+  //Listen for chatMessage
+  socket.on('chatMessage', (msg) => {
+    io.emit('message', formatMessage('USER', msg))
   })
 })
 
